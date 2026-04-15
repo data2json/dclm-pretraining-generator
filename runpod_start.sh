a889df8e5ddb8f67b20ad13391b0ea39f33cbd7e
kstone <essobi@gmail.com>
2026-04-15 04:34:36 +0000

    Install tmux in runpod_start.sh

--- runpod_start.sh
+++ runpod_start.sh
@@ -10,6 +10,7 @@ HF_TOKEN="${HF_TOKEN:?HF_TOKEN required}"
 OUTPUT_REPO="${OUTPUT_REPO:-essobi/dclm-crossover-megadoc-p1}"
 NUM_GPUS="${NUM_GPUS:-8}"
 
+apt-get install -y tmux -qq 2>/dev/null || true
 pip install -q -r requirements.txt
 
 python pipeline.py \
