d37d3f92a66cff391fcd207dbc2c6ea0d2729bb8
Essobi <122826269+data2json@users.noreply.github.com>
2026-04-17 01:18:45 -0400

    Modify pip install to include preserved dependencies
    Updated pip install command to use preserved dependencies.

--- runpod_start.sh
+++ runpod_start.sh
@@ -2,9 +2,9 @@
 set -e # Exit immediately if a command fails
 
 echo "--- Starting Swarm Setup ---"
-
+pip freeze | grep -i torch > preserve.txt
 # 1. Install dependencies
-pip install hf_transfer datasets huggingface_hub pyarrow tqdm arctic-inference==0.1.1 https://github.com/Dao-AILab/flash-attention/releases/download/v2.8.3/flash_attn-2.8.3+cu12torch2.8cxx11abiTRUE-cp312-cp312-linux_x86_64.whl vllm
+pip install hf_transfer datasets huggingface_hub pyarrow tqdm arctic-inference==0.1.1 https://github.com/Dao-AILab/flash-attention/releases/download/v2.8.3/flash_attn-2.8.3+cu12torch2.8cxx11abiTRUE-cp312-cp312-linux_x86_64.whl vllm -c preserve.txt
 # 2. Clone the repo (if not already there)
 git clone https://github.com/data2json/dclm-pretraining-generator.git /workspace/repo || true
 cd /workspace/repo
